fc9bd1abc5cc3bf50a353ba1c0d45558e26e7b07
Cayman Heng <hengcayman@gmail.com>
2021-02-24 10:49:34 -0500

    added border component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Border from "./Components/Border/Border"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Border />
     </div>
   );
 }
---
+++ src/Components/Border/Border.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Border.css";
+
+function Border() {
+  return (
+    <div className="border py-1">
+
+    </div>
+  );
+}
+
+export default Border;
