3e6b987314f08d0a1329bdc4f4c5bb33f5eac703
Cayman Heng <hengcayman@gmail.com>
2021-02-24 10:45:16 -0500

    added header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,2 +1,16 @@
 import React from "react";
 import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header py-5">
+      <h1 className="text-white fs-1">Employee Directory</h1>
+      <p className="text-white">
+        Click on carrots to filter by heading or use the search box to narrow
+        your results.
+      </p>
+    </div>
+  );
+}
+
+export default Header;
