2b33b5b696cbeed1d583ee07d9b2a9b059710b37
Cayman Heng <hengcayman@gmail.com>
2021-02-26 18:02:29 -0500

    mapped employee data and populated data in employee card

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import Border from "./Components/Border/Border";
 import Search from "./Components/Search/Search";
 import EmployeeTable from "./Components/EmployeeTable/EmployeeTable";
 import EmployeeCard from "./Components/EmployeeCard/EmployeeCard";
+import employee from "./employee.json";
 
 //Fetch API
-const url =
-  "https://randomuser.me/api/?results=10&&inc=name,email,dob,phone,picture";
-fetch(url)
-  .then((response) => response.json())
-  .then(function (data) {
-    let record = data.results;
-    renderEmployeeCard(record);
-  });
+// const url =
+//   "https://randomuser.me/api/?results=10&&inc=name,email,dob,phone,picture";
+// fetch(url)
+//   .then((response) => response.json())
+//   .then(function (data) {
+//     let record = data.results;
+//     renderEmployeeCard(record);
+//   });
 
 // const url =
 //   "https://randomuser.me/api/?results=10&&inc=name,email,dob,phone,picture";
@@ -26,23 +27,27 @@ fetch(url)
 // }
 
 //Loop through data from API
-function renderEmployeeCard(record) {
-  const items = [];
-  for (let i = 0; i < record.length; i++) {
-    items.push(
-      <EmployeeCard
-        image={record[i].picture.thumbnail}
-        name={record[i].name.first}
-        phone={record[i].phone}
-        email={record[i].email}
-        dob={record[i].dob.date.substring(0, 10)}
-      />
-    );
-    return items;
-  }
-}
+// function renderEmployeeCard(record) {
+//   const items = [];
+//   for (let i = 0; i < record.length; i++) {
+//     items.push(
+//       <EmployeeCard
+//         image={record[i].picture.thumbnail}
+//         name={record[i].name.first}
+//         phone={record[i].phone}
+//         email={record[i].email}
+//         dob={record[i].dob.date.substring(0, 10)}
+//       />
+//     );
+//     return items;
+//   }
+// }
 
 class App extends Component {
+  state = {
+    employee
+  };
+
   render() {
     return (
       <div className="App">
@@ -50,7 +55,16 @@ class App extends Component {
         <Border />
         <Search />
         <EmployeeTable />
-        <EmployeeCard />
+        {this.state.employee.map(employee => (
+          <EmployeeCard 
+          image={employee.picture.large}
+          name={employee.name.first}
+          phone={employee.phone}
+          email={employee.email}
+          dob={employee.dob.date.substring(0, 10)}
+          key={employee.email}
+          />
+        ))}
       </div>
     );
   }
